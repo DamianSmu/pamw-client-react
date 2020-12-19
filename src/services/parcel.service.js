b0cc2da54ba21e2c3eeee3a03dffc81e73354c56
DamianSmu <smugorzewski.ds@gmail.com>
2020-12-19 20:46:30 +0100

    Heroku buildpack

--- src/services/parcel.service.js
+++ src/services/parcel.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from '../auth-header';
 
 
-const API_URL = process.env.API_BASE_URL + "parcel/";
+const API_URL = process.env.REACT_APP_API_BASE_URL + "parcel/";
 
 class ParcelService {
 
